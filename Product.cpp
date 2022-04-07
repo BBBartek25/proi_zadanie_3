b6e2ac6372c1d0bced596de53a4e2d0febac4f38
Bartosz Gryszkiewicz <01168884@pw.edu.pl>
2022-04-07 20:13:30 +0200

    Creating basics of product class

--- Product.cpp
+++ Product.cpp
@@ -6,11 +6,41 @@
 class product {
 	int mass;
 	std::string name;
-	producer producer;
+	producer food_producer;
 	int number;
 	std::tm expDate;
 	std::vector<ingredient> ingredients;
 public:
+	int Mass() {
+		return mass;
+	}
+	std::string Name() {
+		return name;
+	}
+	producer Producer() {
+		return food_producer;
+	}
+	int Number() {
+		return number;
+	}
+	std::tm ExpDate() {
+		return expDate;
+	}
+	std::vector<ingredient> Ingredients() {
+		return ingredients;
+	}
+	void setMass(int nMass) {
+		mass = nMass;
+	}
+	void setName(std::string nName) {
+		name = nName;
+	}
+	void setProducer(producer nProducer) {
+		food_producer = nProducer;
+	}
+	void setNumber(int nNumber) {
+		number = nNumber;
+	}
 
 };
 
